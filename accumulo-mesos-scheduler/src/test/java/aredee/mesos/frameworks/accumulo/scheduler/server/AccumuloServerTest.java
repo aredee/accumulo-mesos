c4e7f6b045d4162c3f51621cec847e7178ac78ce
klucar <klucar@gmail.com>
2015-07-28 11:50:03 -0400

    hand merge of master work

---
+++ accumulo-mesos-scheduler/src/test/java/aredee/mesos/frameworks/accumulo/scheduler/server/AccumuloServerTest.java
@@ -0,0 +1,8 @@
+package aredee.mesos.frameworks.accumulo.scheduler.server;
+
+/**
+ * Created by klucar on 5/11/15.
+ */
+public class AccumuloServerTest {
+    //TODO (klucar) test equals and hashCode
+}
