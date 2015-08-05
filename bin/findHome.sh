7c9f1539ffd0a4a60bb98f9526d851123e4afdfc
Paul Read <pdread101@gmail.com>
2015-08-05 13:00:31 -0400

    added scripts and confs to distro

---
+++ bin/findHome.sh
@@ -0,0 +1,16 @@
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+export SCRIPT_HOME=`cd "$PRGDIR/.." ; pwd`
